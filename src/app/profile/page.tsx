5b732ebbc4b2724fd64078beb28b6874bc854735
Libo033 <113865453+Libo033@users.noreply.github.com>
2024-02-07 11:27:55 -0300

    En proceso: profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,6 +3,7 @@ import React, { useContext } from "react";
 import styles from "../page.module.css";
 import { AuthContext } from "@/context/AuthContext";
 import NoAccount from "@/components/auth/NoAccount";
+import { Button, TextField } from "@mui/material";
 
 const Profile = () => {
   const { loaded, user } = useContext(AuthContext);
@@ -12,6 +13,70 @@ const Profile = () => {
       {loaded && user ? (
         <div className="page2">
           <p className={styles.PageTitle}>Perfil</p>
+          <div className={styles.Profile_Info}>
+            <div style={{ display: "flex", alignItems: "center", gap: "16px" }}>
+              <TextField
+                sx={{ minWidth: "300px", width: "30%" }}
+                label="email"
+                value={user.email}
+                disabled
+              />
+              {user.emailVerified ? (
+                <p
+                  style={{
+                    color: "#67a26a",
+                    fontSize: "large",
+                    fontWeight: "300",
+                  }}
+                >
+                  Email verificado
+                </p>
+              ) : (
+                <Button
+                  sx={{ minWidth: "230px" }}
+                  variant="contained"
+                  size="large"
+                >
+                  Verificar email
+                </Button>
+              )}
+            </div>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                gap: "16px",
+                marginTop: "36px",
+              }}
+            >
+              <TextField
+                sx={{ minWidth: "300px", width: "30%" }}
+                type="password"
+                value={"**************"}
+                label="contraseña"
+                disabled
+              />
+              <Button
+                sx={{ minWidth: "230px" }}
+                variant="contained"
+                size="large"
+              >
+                Cambiar contraseña
+              </Button>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                gap: "16px",
+                marginTop: "36px",
+              }}
+            >
+              <Button color="error" variant="contained" size="large">
+                Eliminar Cuenta
+              </Button>
+            </div>
+          </div>
         </div>
       ) : (
         <div className="page2">
