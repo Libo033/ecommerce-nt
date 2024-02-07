9cb93994120cf46f9aec65c9318f162a8840a41e
Libo033 <113865453+Libo033@users.noreply.github.com>
2024-02-07 12:27:56 -0300

    En proceso: prods page

--- src/app/prods/page.tsx
+++ src/app/prods/page.tsx
@@ -1,10 +1,18 @@
+import { Breadcrumbs } from "@mui/material";
+import Link from "next/link";
 import React from "react";
 
 const Prods = () => {
   return (
     <div style={{ backgroundColor: "#fafafa" }} className="page2">
-      <div></div>
-      <div></div>
+      <Breadcrumbs aria-label="breadcrumb">
+        <Link className="LinkA" href={"/"}>INICIO</Link>
+        <span className="Breadcrumb_Span">PRODUCTOS</span>
+      </Breadcrumbs>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 };
