d1394caac26e26b8ec63f56fdb580db288e73f81
Libo033 <113865453+Libo033@users.noreply.github.com>
2024-02-19 10:33:21 -0300

    Creado: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
+import { jwtVerify } from "jose";
 
 export async function middleware(request: NextRequest) {
-  await fetch("http://localhost:3000/api/auth/is_admin");
+  const mySession = request.cookies.get("mySession");
+  const secret_key: Uint8Array = new TextEncoder().encode(
+    process.env.JWT_SECRET
+  );
 
-  if (true) {
-    return NextResponse.next();
+  if (mySession) {
+    const { payload } = await jwtVerify(mySession.value, secret_key);
+    const res = await fetch(
+      `http://localhost:3000/api/auth/is_admin/${payload.uid}`
+    );
+    const { isAdmin }: { isAdmin: boolean } = await res.json();
+
+    if (isAdmin) return NextResponse.next();
   }
 
   return NextResponse.redirect(new URL("/", request.url));
