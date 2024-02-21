3c74e1597481a5f0a88bdcab873915c11fae6e2d
Libo033 <113865453+Libo033@users.noreply.github.com>
2024-02-21 10:11:23 -0300

    Creado: API prods GET all y POST

--- src/app/api/prods/route.ts
+++ src/app/api/prods/route.ts
@@ -5,7 +5,11 @@ import { jwtVerify } from "jose";
 
 export async function GET(req: Request) {
   try {
-    // GET ONE
+    const client: MongoClient = await clientPromise;
+    const db: Db = client.db("ecommerce");
+    const productos = await db.collection("productos").find().toArray();
+
+    return Response.json({ code: 200, productos }, { status: 200 });
   } catch (error) {
     if (error instanceof Error) {
       return Response.json({ code: 500, Error: error }, { status: 500 });
@@ -28,7 +32,34 @@ export async function POST(req: NextRequest) {
       const { isAdmin }: { isAdmin: boolean } = await res.json();
 
       if (isAdmin) {
-        // POST
+        const client: MongoClient = await clientPromise;
+        const db: Db = client.db("ecommerce");
+        const data = await req.json();
+
+        const new_product = {
+          marca: data.marca,
+          detalle: data.detalle,
+          img: data.img,
+          categoria: data.categoria,
+          precio: data.precio,
+          stock: data.stock,
+          mostrar: data.mostrar,
+          otros: data.otros,
+          genero: data.genero,
+        };
+
+        const created_product = await db
+          .collection("productos")
+          .insertOne(new_product);
+
+        return Response.json(
+          {
+            code: 201,
+            created: created_product.acknowledged,
+            id: created_product.insertedId,
+          },
+          { status: 201 }
+        );
       } else {
         return Response.json(
           { code: 403, Error: "invalid_token" },
